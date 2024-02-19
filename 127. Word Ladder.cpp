d69d029cf5f4f78644ee3c644b2a2a3ba8f51a59
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-19 12:46:34 +0530

    Word Ladder

---
+++ 127. Word Ladder.cpp
@@ -0,0 +1,29 @@
+Q.127. Word Ladder
+link-->https://leetcode.com/problems/word-ladder/description/
+code--> int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        queue<pair<string , int>>q;
+        q.push({beginWord , 1});
+        unordered_set<string>s(wordList.begin() , wordList.end());
+        s.erase(beginWord);
+        while(!q.empty()){
+            string word= q.front().first;
+            int steps= q.front().second;
+            q.pop();
+            if(word==endWord){
+                return steps;
+            }
+            for(int i=0;i<word.size();i++){
+                char original= word[i];
+                for(char ch ='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(s.find(word)!=s.end()){
+                        s.erase(word);
+                        q.push({word , steps+1});
+                    }
+                }
+                word[i]=original;
+            }
+        }
+        return 0;
+    }
